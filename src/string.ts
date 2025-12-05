9f777defcd1949dfea35493b7b0690fab66ab15d
Roger Pence <roger.pence@gmail.com>
2025-12-05 16:15:14 -0600

    add getSlug to docs

--- src/string.ts
+++ src/string.ts
@@ -3,12 +3,12 @@ import path from "path";
 /**
  * Get a slug from folder name and full file name.
  * @param {string} filePath
- * @param {string} filename
+ * @param {string} fileName
  *
  *
  * @returns {string}
  */
-export function getSlug(filePath: string, filenName: string): string {
+export function getSlug(filePath: string, fileName: string): string {
     const folder = filePath.split("\\").at(-1) ?? "";
-    return `${folder}/${filenName.split(".")[0]}`;
+    return `${folder}/${fileName.split(".")[0]}`;
 }
